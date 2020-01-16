e15c58d7f08a653d744245c2f13609cf8c8f0130
AatreshKulkarni <aatresh.kulkarni08@gmail.com>
2020-01-16 16:45:38 +0530

    create new project

---
+++ app.js
@@ -0,0 +1,29 @@
+var connectionString = '[IoT Hub device connection string]';
+ 
+// use factory function from AMQP-specific package
+var clientFromConnectionString = require('azure-iot-device-amqp').clientFromConnectionString;
+ 
+// AMQP-specific factory function returns Client object from core package
+var client = clientFromConnectionString(connectionString);
+ 
+// use Message object from core package
+var Message = require('azure-iot-device').Message;
+ 
+var connectCallback = function (err) {
+  if (err) {
+    console.error('Could not connect: ' + err);
+  } else {
+    console.log('Client connected');
+    var msg = new Message('some data from my device');
+    client.sendEvent(msg, function (err) {
+      if (err) {
+        console.log(err.toString());
+      } else {
+        console.log('Message sent');
+      };
+    });
+  };
+};
+ 
+ 
+client.open(connectCallback);
